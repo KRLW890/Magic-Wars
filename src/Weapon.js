8245fc4f93b46ecbcd93fa41cc1343dded233c98
KRLW890 <31046367+KRLW890@users.noreply.github.com>
2020-08-11 10:34:48 -0500

    Update Weapon.js

--- src/Weapon.js
+++ src/Weapon.js
@@ -1,24 +1,24 @@
 var staggering = false;
 
-var Weapon = function(name, img, baseDamage, atk, def, stagger)
+var Weapon = function(name, img, baseDamage, atk, def, stagger, statReqs)
 {
     this.name = name;
     this.img = img;
     this.baseDamage = baseDamage;
     this.atk =  atk;
     this.def = def;
+    this.infoType = "weapon";
     this.formula = function(user, opp)
-    {console.log(this.baseDamage + " * " +(user.getStatById(this.atk) + user.atk/2) + " / " + (opp.getStatById(this.def) + opp.def/2));
-console.log(constrain(Math.floor( this.baseDamage *
-               (user.getStatById(this.atk) + user.atk/2) /
-               (opp.getStatById(this.def) + opp.def/2)
-               ), 1, Infinity));
+    {
         return constrain(Math.floor( this.baseDamage *
                (user.getStatById(this.atk) + user.atk/2) /
                (opp.getStatById(this.def) + opp.def/2)
                ), 1, Infinity);
     };
+    //this.staggerChance = stagger || function() { return 0 };
     this.stagger = stagger || function() { return false; };
+//function(userStats, oppStats) { return (random() <= constrain((userStats.strd-oppStats.strd)/oppStats.strd*, 0, 0.3)); }
+    this.statReqs = statReqs || new Stats(0);
     this.pid;
     this.attack = function(pid) { this.pid = pid; players[pid].animations.attack(); };
     this.drawAttack = function() { players[this.pid].animations.drawAttack(); };
@@ -26,10 +26,11 @@ console.log(constrain(Math.floor( this.baseDamage *
 
 function initWeapons() {
     weapons = [
-        new Weapon("Rapier", images.rapier, 10, 1, 2),
+        new Weapon("Rapier", images.rapier, 9, 1, 2),
         new Weapon("Lance", images.lance, 12, 0, 3, function(userStats, oppStats) { return (random() <= constrain((userStats.strd-oppStats.strd)/oppStats.strd*0.3, 0, 0.3)); }),
-        new Weapon("Musket", images.musket, 15, 1, 2),
-        new Weapon("Grisner's Warhammer", images.grisnersWarhammer, 20, 0, 3, function(userStats, oppStats) { return (random() <= constrain((userStats.strd-oppStats.strd)/oppStats.strd*0.2, 0, 0.2)); }),
+        new Weapon("Musket", images.musket, 15, 1, 2, function() { return false; }, new Stats(0, 120, 0, 0)),
+        new Weapon("Grisner's Warhammer", images.grisnersWarhammer, 25, 0, 3, function(userStats, oppStats) { return (random() <= constrain((userStats.strd-oppStats.strd)/oppStats.strd*0.2, 0, 0.2)); }, new Stats(0, 0, 0, 200)),
+        new Weapon("Sword", images.sword, 12, 0, 2),
         //new Weapon("Bow & Arrow", images.lance, 12, new Stats(0, 0.2, 0.9, 0.3, 0.3, 0.4), 0),
     ];
     weapons[2].attack = function(pid)
